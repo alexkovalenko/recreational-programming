23d2609076ce3c992d1f8790a7453e4ba15c891b
Alex Kovalenko <alex.kov.job@gmail.com>
2021-06-06 12:22:48 +0000

    Sync LeetCode submission - Squares of a Sorted Array (java)

---
+++ problems/squares_of_a_sorted_array/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        return Arrays.stream(nums)
+            .map(n -> n * n)
+            .sorted()
+            .toArray();
+    }
+}
