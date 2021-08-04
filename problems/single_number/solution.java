9fe36699c665d870f292293eb95266720910cdd9
Alex Kovalenko <alex.kov.job@gmail.com>
2021-08-04 17:56:33 +0000

    Sync LeetCode submission - Single Number (java)

--- problems/single_number/solution.java
+++ problems/single_number/solution.java
@@ -1,15 +1,5 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        /*Arrays.sort(n);
-        for (int i = 0; i < n.length - 1; i++) {
-            if (i == 0 && n[i] != n[i + 1]) {
-                return n[i];
-            }    
-            if (i > 0 && n[i] != n[i - 1] && n[i] != n[i + 1]) {
-                return n[i];
-            }
-        }
-        return n[n.length - 1];*/
         int mask = 0;
         for(int n : nums) {
             mask ^= n;
