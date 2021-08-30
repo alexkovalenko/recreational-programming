7c2f2c95c304cd4cda74162a3f87574c5d966101
Alex Kovalenko <alex.kov.job@gmail.com>
2021-08-30 11:36:42 +0000

    Sync LeetCode submission - Move Zeroes (java)

---
+++ problems/move_zeroes/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] sorted = new int[nums.length];
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                sorted[index++] = num;
+            }
+        }
+        System.arraycopy(sorted, 0, nums, 0, nums.length);
+    }
+}
