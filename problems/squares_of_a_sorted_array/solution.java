930ad65c0b06d9f5bf96ca8b49a022a23c9a3ade
Alex Kovalenko <alex.kov.job@gmail.com>
2021-09-04 14:20:15 +0000

    Sync LeetCode submission - Squares of a Sorted Array (java)

--- problems/squares_of_a_sorted_array/solution.java
+++ problems/squares_of_a_sorted_array/solution.java
@@ -1,8 +1,18 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        return Arrays.stream(nums)
-            .map(n -> n * n)
-            .sorted()
-            .toArray();
+        int len = nums.length;
+        int[] squares = new int[len];
+        int left = 0;
+        int right = len - 1;
+        for (int i = len - 1; i > -1 ; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                squares[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                squares[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return squares;
     }
 }
